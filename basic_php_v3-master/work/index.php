e58f76a0a5f1fdfc3c8706b4763a1b3d7cb9fd89
TAKAHIRO <takahiro1109tm568@gmail.com>
2021-05-13 21:33:23 +0900

    部分テンプレートの作成

--- basic_php_v3-master/work/index.php
+++ basic_php_v3-master/work/index.php
@@ -1,57 +1,9 @@
-<!-- PHPのコードをHTML外に定義して分かりやすくする -->
+<!-- phpの開始終了タグとファイルを読み込むinckude -->
 <?php
-// ランダムなｎを生成してその数値により表示するメッセージを変更
-$n = mt_rand(1 , 3);
-// スクリプトタブでアラートを表示させている。
-if($n === 1){
-  $message = '<script>alert(1);</script>UI/UXデザイナー見習いです';
-} elseif ($n === 2){
-  $message = 'プログラミング勉強中です';
-} else {
-  $message = 'PHPちょっと分かります';
-}
 
+include('_header.php');
 
 ?>
-
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="utf-8">
-  <title>太郎のポートフォリオサイト</title>
-  <link rel="icon" href="favicon.ico">
-  <meta name="description" content="太郎のポートフォリオサイトです。">
-  <link rel="stylesheet" href="css/styles.css">
-</head>
-<body>
-  <header>
-    <div class="container">
-      <div class="icon">
-        <img src="img/taro.png" width="120" height="120" alt="太郎のアイコンです">
-      </div>
-      <div class="info">
-        <h1>山田太郎</h1>
-        <!-- ↓HTMLにPHPのコードを埋め込む -->
-        <!-- スクリプトタグのアラートを文字列とする。+文字の種別はUTFである。 -->
-        <p><?= htmlspecialchars($message, ENT_QUOTES, 'UTF-8'); ?></p>
-        <ul>
-          <li>
-            <a href="https://dotinstall.com" target="_blank">
-              <img src="img/blog.png" width="20" height="20" alt="ブログサイトへのリンク画像です">
-            </a>
-          </li>
-          <li>
-            <a href="https://dotinstall.com" target="_blank">
-              <img src="img/photos.png" width="20" height="20" alt="写真サイトへのリンク画像です">
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </header>
-
-  <section class="works">
-    <h1>WORKS</h1>
     <section>
       <img src="img/work1.png" width="400" height="260" alt="勇者ゲームの紹介画像">
       <h1>勇者ゲーム</h1>
@@ -75,11 +27,7 @@ if($n === 1){
 
     </section>
 
-  </section>
+<!-- phpの終了タグは後にHTMLの記載がない場合は省略可 -->
+<?php
 
-  <footer>
-    <p>(c) dotinstall.com</p>
-  </footer>
-</body>
-</html>
-  
+include('_footer.php');
